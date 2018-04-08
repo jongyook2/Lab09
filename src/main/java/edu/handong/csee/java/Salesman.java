900143d96401c3342671443ccdb93a679b684ab0
Yoon Jong Wook <emrrn92@naver.com>
2018-04-08 22:10:58 +0900

    add javadoc (Salesman class)

--- src/main/java/edu/handong/csee/java/Salesman.java
+++ src/main/java/edu/handong/csee/java/Salesman.java
@@ -1,20 +1,41 @@
-package edu.handong.csee.java;
-
+package edu.handong.csee.java; //package name
+/**
+ * This class defines Salesman object.<br>
+ * It is used for array(team) , has getter and setter for member names and sales 
+ * @author YJW <br>
+ * 
+ */
 public class Salesman {
 	
-	private String mName; //member 
-	private double sales;
+	private String mName; //member's name
+	private double sales; //member's sales
+	/**
+	 * This method is mName getter of Salesman Class <br>
+	 * @return mName
+	 */
 	public String getmName() {
-		return mName;
+		return mName; // getmName() do return mName
 	}
+	/**
+	 * This method is mName setter of Salesman Class <br>
+	 * @parameter String mName
+	 */
 	public void setmName(String mName) {
-		this.mName = mName;
+		this.mName = mName; //store mName in this.mName
 	}
+	/**
+	 * This method is Sales getter of Salesman Class <br>
+	 * @return sales
+	 */
 	public double getSales() {
-		return sales;
+		return sales; //getSales() do return sales
 	}
+	/**
+	 * This method is Sales setter of Salesman Class <br>
+	 * @parameter double sales
+	 */
 	public void setSales(double sales) {
-		this.sales = sales;
+		this.sales = sales; //store sales in this.sales
 	}
 	
 	
