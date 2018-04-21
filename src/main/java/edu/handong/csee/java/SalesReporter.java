408ef52ea3ce73ae69085075dd0bc0245c7dc274
Yoon Jong Wook <emrrn92@naver.com>
2018-04-21 19:55:38 +0900

    last check

--- src/main/java/edu/handong/csee/java/SalesReporter.java
+++ src/main/java/edu/handong/csee/java/SalesReporter.java
@@ -15,7 +15,7 @@ public class SalesReporter {
 	private double highestSales; //declare instance variable(highest sales record)
 	private double averageSales; //declare instance variable(average sales record)
 	ArrayList<Salesman> team = new ArrayList(); //instantiate team(ArrayList<Saleman>
-	
+
 	/**
 	 * This public static void method is main <br>
 	 * instantiates SalesReporter Class to Reporting <br>
@@ -34,12 +34,12 @@ public class SalesReporter {
 	 * initializes Salesman to mySalesman and stores name and sales in mySalesman then, mySalesmna add to team(arrayList)
 	 */
 	public void getData() {
-		 
+
 		int i=0; //local variable(use index)
 		while(true){ //loop until choose equal to "No"
 			Scanner keyboard= new Scanner(System.in); // instantiate Class Scanner to keyboard(input)
 			System.out.println("Enter data for associate number "+(i+1)); //output(Enter data for associate number 1~numberofSalesman ) is shown in your monitor
-			i++;
+			i++; // increase(it is index of Salesman)
 			System.out.print("Enter name of sales associate: "); //output(Enter number of sales associates: ) is shown in your monitor
 			String name = keyboard.nextLine(); //name is input(string type) from keyboard
 			System.out.print("Enter assoiate's sales: $"); //output(Enter assoiate's sales: $ ) is shown in your monitor
@@ -53,11 +53,11 @@ public class SalesReporter {
 			System.out.println(); //next line
 			System.out.print("Do you want to input others?(yes or no) "); //output(Do you want to input others?(yes or no) ) is shown in your monitor
 			String choose = keyboard.nextLine(); //choose is input(string type) from keyboard
-			
+
 			if(choose.equalsIgnoreCase("no")){ //if choose equal to "no" 
 				break; //escape from while loop
 			}
-			
+
 		}
 	}
 	/**
@@ -103,20 +103,20 @@ public class SalesReporter {
 			}
 		}
 		if(team.size()>1) {
-		System.out.println("\nThe rest performed as follows:"); //output(The rest performed as follows:) is shown in your monitor
-		for(i=0; i < team.size(); i++) { //loop 0 to array length-1
-			if(team.get(i).getSales() != highestSales) { //if team.get(i)'s Sales is not equal to highestSales
-				System.out.println("Name: " + team.get(i).getmName()); //output(Name: team.get(i)'s name) is shown in your monitor
-				System.out.println("Sales: $" +team.get(i).getSales()); //output(Sales: team.get(i)'s Sales) is shown in your monitor
-				difference = team.get(i).getSales() - averageSales; //initialize to (team.get(i)'s sales-average) 
-				if(difference > 0) //if difference is larger than 0
-					System.out.println("$"+difference+" is above the average."); //output($difference is above the average) is shown in your monitor
-				else if(difference < 0) //if difference is less than 0
-					System.out.println("$"+Math.abs(difference)+" is below the average."); //output($difference is below the average) is shown in your monitor
-				else //if difference is equal to 0
-					System.out.println("sales is the average(same)."); //output(sales is the average(same).) is shown in your monitor
+			System.out.println("\nThe rest performed as follows:"); //output(The rest performed as follows:) is shown in your monitor
+			for(i=0; i < team.size(); i++) { //loop 0 to arraylist size-1
+				if(team.get(i).getSales() != highestSales) { //if team.get(i)'s Sales is not equal to highestSales
+					System.out.println("Name: " + team.get(i).getmName()); //output(Name: team.get(i)'s name) is shown in your monitor
+					System.out.println("Sales: $" +team.get(i).getSales()); //output(Sales: team.get(i)'s Sales) is shown in your monitor
+					difference = team.get(i).getSales() - averageSales; //initialize to (team.get(i)'s sales-average) 
+					if(difference > 0) //if difference is larger than 0
+						System.out.println("$"+difference+" is above the average."); //output($difference is above the average) is shown in your monitor
+					else if(difference < 0) //if difference is less than 0
+						System.out.println("$"+Math.abs(difference)+" is below the average."); //output($difference is below the average) is shown in your monitor
+					else //if difference is equal to 0
+						System.out.println("sales is the average(same)."); //output(sales is the average(same).) is shown in your monitor
+				}
 			}
 		}
 	}
-}
 }
